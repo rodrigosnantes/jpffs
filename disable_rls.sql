74e5814b96bebb326880f3215409646c4308d3d4
Rodrigo Nantes <rodrigo@fractaleng.com.br>
2026-03-17 19:58:31 -0300

    fix: removido codigo nao mais necessário

--- disable_rls.sql
+++
@@ -1,8 +0,0 @@
--- DISABLE ROW LEVEL SECURITY (The "Nuclear Option" to fix 401 errors)
-alter table profiles disable row level security;
-alter table players disable row level security;
-alter table matches disable row level security;
-alter table match_events disable row level security;
-
--- Verify it worked by checking policies (Optional, mainly for you to know)
--- If this runs successfully, you should be able to insert data immediately.
